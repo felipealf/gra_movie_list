c2c3cbc88517b6b167c768db6445a18fcf2dc4c2
Felipe Almeida <felipe.almeif@gmail.com>
2025-03-18 16:23:03 -0300

    update integration test comparing default archive values

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,32 +1,48 @@
 const request = require('supertest');
 const { describe, it, before } = require('mocha');
 const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 const app = require('../src/index');
+const expectedMoviesPath = path.join(__dirname, 'expected_movies.json');
+const expectedProducersPath = path.join(__dirname, 'expected_producers.json');
+
+function checkFilesExistence() {
+    if (!fs.existsSync(expectedMoviesPath) || !fs.existsSync(expectedProducersPath)) {
+        console.error('Os arquivos de referência esperados não existem. Verifique o README.md para saber como gerá-los.');
+        process.exit(1);
+    }
+}
+
+checkFilesExistence();
+
+const expectedMovies = JSON.parse(fs.readFileSync(path.join(__dirname, 'expected_movies.json'), 'utf8'));
+const expectedProducers = JSON.parse(fs.readFileSync(path.join(__dirname, 'expected_producers.json'), 'utf8'));
 
 describe('Testes de Integração', function () {
-    before((done) => {
-        setTimeout(done, 1000);
+    before(function (done) {
+        this.timeout(10000);
+        setTimeout(done, 5000);
     });
 
-    it('Deve retornar a lista de filmes', function (done) {
+    it('Deve retornar a lista de filmes corretamente', function (done) {
         request(app)
             .get('/movies')
             .expect('Content-Type', /json/)
             .expect(200)
             .end((err, res) => {
-                assert.ok(res.body.length > 0);
+                assert.deepStrictEqual(res.body, expectedMovies, 'Os filmes retornados não correspondem ao esperado');
                 done(err);
             });
     });
 
-    it('Deve retornar os produtores com maior e menor intervalo entre vitórias', function (done) {
+    it('Deve retornar os produtores com maior e menor intervalo corretamente', function (done) {
         request(app)
             .get('/producers/intervals')
             .expect('Content-Type', /json/)
             .expect(200)
             .end((err, res) => {
-                assert.ok(res.body.max);
-                assert.ok(res.body.min);
+                assert.deepStrictEqual(res.body, expectedProducers, 'Os produtores retornados não correspondem ao esperado');
                 done(err);
             });
     });
